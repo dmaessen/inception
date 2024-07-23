e4593288a9a2df88c2d7dc15185a439c76b5c279
Dominique Maessen <dmaessen@f1r2s13.codam.nl>
2024-07-23 12:35:55 +0200

    changes made

--- srcs/requirements/wordpress/wp_conf.sh
+++ srcs/requirements/wordpress/wp_conf.sh
@@ -15,5 +15,29 @@ chown -R wwww-data:wwww-data /var/wwww/wordpress
 #---------------------------------------------------ping mariadb---------------------------------------------------#
 # check if mariadb container is up and running, trying to connect to its port
 ping_mariadb_container() {
-
+    # netcat scans via the port to see if connection with mariadb database is open
+    # port 3306 which is the default for the MySQL and mariadb database servers
+    # /dev/null meaning no output will be showed on stdout unless an error
+    nc -zv mariadb 3306 > /dev/null
+    return $? # exit status of ping
 }
+start_time$(date +%s) # current time in seconds
+end_time=$((start)_time + 20) # adds 20 sec to start time
+while [$(date + $s) -lt $end_time]; do # loops until current time bigger than end (-lt = less than, it compares)
+    ping_mariadb_container
+    if [$? -eq 0]; then # check if ping succesful
+        echo "[---- MARIADB IS UP AND RUNNING ----]"
+        break
+    else
+        echo "[----- WAITING FOR MARIADB -----]"
+        sleep 1 # tries again after 1sec
+    fi
+done
+
+if [$(date + $s) -ge $end_time]; then # checks if current time is greater or equal to end
+    echo "[---- NO RESPONSE FROM MARIADB ----]"
+fi
+
+#---------------------------------------------------wp installation---------------------------------------------------#
+# downloads wordpress core files
+wp core download --allow-root
