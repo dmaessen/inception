38815f921880f041c63d497ead666e892f51ce89
Dominique Maessen <dmaessen@f1r1s13.codam.nl>
2024-10-14 16:39:23 +0200

    changes made

--- srcs/requirements/mariadb/tools/create_mdb.sh
+++ srcs/requirements/mariadb/tools/create_mdb.sh
@@ -1,43 +1,44 @@
-#!/bin/bash
+!/bin/bash
 
-# # checks if databases exisits and creates it if it doesn't
-# if [ ! -d "/var/lib/mysql/$MDB_NAME" ]; then
-#     mariadb-install-db --user=mysql --datadir=/var/lib/mysl --skip-test-db --skip-name-resolve --auth-root-authentication-method=normal
+# checks if databases exisits and creates it if it doesn't
+if [ ! -d "/var/lib/mysql/$MDB_NAME" ]; then
+    # mariadb-install-db --user=mysql --datadir=/var/lib/mysl --skip-test-db --skip-name-resolve --auth-root-authentication-method=normal
 
-#     # starts mariadb service temporarly to create the database and user
-#     service mariadb start
+    # starts mariadb service temporarly to create the database and user
+    service mysql start;
 
-#     # passes argument as string, grants full access (SELECT/INSERT/UPDATE/DEL..) on all databases/tables
-#     mysql -e "
-#         CREATE DATABASE IF NOT EXISTS $MDB_NAME;
-#         CREATE USER IF NOT EXISTS '$MDB_USER'@'%' IDENTIFIED BY '$MDB_PASSWORD';
-#         ALTER USER '$MDB_USER'@'%' IDENTIFIED BY '$MDB_PASSWORD';
-#         GRANT ALL PRIVILEGES ON *.* TO '$MDB_USER'@'%' IDENTIFIED BY '$MDB_PASSWORD';
-#         FLUSH PRIVILEGES;
-#     "
-#     service mariadb stop
+    # passes argument as string, grants full access (SELECT/INSERT/UPDATE/DEL..) on all databases/tables
+    mysql -e "
+        CREATE DATABASE IF NOT EXISTS $MDB_NAME;
+        CREATE USER IF NOT EXISTS '$MDB_USER'@'%' IDENTIFIED BY '$MDB_PASSWORD';
+        ALTER USER '$MDB_USER'@'%' IDENTIFIED BY '$MDB_PASSWORD';
+        GRANT ALL PRIVILEGES ON *.* TO '$MDB_USER'@'%' IDENTIFIED BY '$MDB_PASSWORD';
+        FLUSH PRIVILEGES;
+    "
+    # service mariadb stop
+    mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
-# fi
+fi
 
-# echo "MariaDB is ready!";
-# exec "$@"
+echo "MariaDB is ready!";
+exec "$@"
 
 
 
 # Check if the specified database (defined in .env) directory exists
-if [ -d "/var/lib/mysql/$MDB_NAME" ]; then 
-    echo "Database already exists"
-else
-    {
-        echo "FLUSH PRIVILEGES;"
-        echo "CREATE DATABASE IF NOT EXISTS $MDB_NAME;"
-        echo "CREATE USER IF NOT EXISTS $MDB_USER@'%' IDENTIFIED BY '$MDB_PASS';"
-        echo "GRANT ALL ON *.* TO $MDB_USER@'%' IDENTIFIED BY '$MDB_PASS';"
-        echo "FLUSH PRIVILEGES;"
-    } | mysqld --bootstrap
-fi
+# if [ -d "/var/lib/mysql/$MDB_NAME" ]; then 
+#     echo "Database already exists"
+# else
+#     {
+#         echo "FLUSH PRIVILEGES;"
+#         echo "CREATE DATABASE IF NOT EXISTS $MDB_NAME;"
+#         echo "CREATE USER IF NOT EXISTS $MDB_USER@'%' IDENTIFIED BY '$MDB_PASS';"
+#         echo "GRANT ALL ON *.* TO $MDB_USER@'%' IDENTIFIED BY '$MDB_PASS';"
+#         echo "FLUSH PRIVILEGES;"
+#     } | mysqld --bootstrap
+# fi
 
-echo "executing mysql daemon"
-exec mysqld
-# using exec allows child processes to recieve the sigterm from docker stop,
+# echo "executing mysql daemon"
+# exec mysqld_safe
+# # using exec allows child processes to recieve the sigterm from docker stop,
 # allowing for clean shutdowns
